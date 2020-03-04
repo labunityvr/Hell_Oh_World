91934eb8790c270dda976e46bf1915e574303ddf
gothikieros <36310006+gothikieros@users.noreply.github.com>
2020-03-04 20:13:06 +0200

    The First Step
    This is the first Stable Master We have. Clone it And mess around.
    This is up to the "Cammera Follows Player" Video.
    
    I made this neat and tidy. Every script group has a folder, every material group has a folder. If possible, try to keep it that way. :)
    
    Good Luck!

---
+++ Our Project/Assets/Scripts/Camera_Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Declaring public variables:
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Our Project/Assets/Scripts/Player_Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+// We made a class "PlayerMovement" that inherits from the 
+// "MonoBehaviour" class. We need to look it up!
+public class PlayerMovement : MonoBehaviour
+{
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000F;
+    public float sidewaysForce = 500F;
+
+    // We marked this as "FixedUpdate" because we
+    // are using it to mess with PHYSICS
+    void FixedUpdate()
+    {  
+        // Added forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // Added Player input (Basic)
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
